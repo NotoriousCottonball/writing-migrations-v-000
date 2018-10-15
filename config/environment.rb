6b8c5ea47dcb976415b68138c82056d3633c5454
George  Pianka <gpianka1@gmail.com>
2018-10-15 06:39:42 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 require 'bundler/setup'
 Bundler.require
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
-Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+#Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+#Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "../lib/**/*.rb")].each { |f| require f }
 
 ENV["SCHOOL_ENV"] ||= "development"
 
